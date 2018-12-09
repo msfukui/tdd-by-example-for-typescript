5ce70508e734811599c757067934b7f169a93026
msfukui <msfukui@gmail.com>
2018-12-09 19:43:11 +0900

    Impl 'Equality for All, Redux'

--- src/money.ts
+++ src/money.ts
@@ -1,35 +1,32 @@
 namespace Money {
-  export class Dollar {
-    private amount: number = 0;
+  export class Money {
+    protected amount: number = 0;
 
+    public equals(object: any): boolean {
+      const money = object as Money;
+      return this.amount === money.amount;
+    }
+  }
+
+  export class Dollar extends Money {
     constructor(amount: number) {
+      super();
       this.amount = amount;
     }
 
     public times(multiplier: number): Dollar {
       return new Dollar(this.amount * multiplier);
     }
-
-    public equals(object: any): boolean {
-      const dollar = object as Dollar;
-      return this.amount === dollar.amount;
-    }
   }
 
-  export class Franc {
-    private amount: number = 0;
-
+  export class Franc extends Money {
     constructor(amount: number) {
+      super();
       this.amount = amount;
     }
 
     public times(multiplier: number): Franc {
       return new Franc(this.amount * multiplier);
-    }
-
-    public equals(object: any): boolean {
-      const dollar = object as Franc;
-      return this.amount === dollar.amount;
     }
   }
 }
