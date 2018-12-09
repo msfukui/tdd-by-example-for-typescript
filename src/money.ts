6e95059ba5b00382abde6a41c87a300d673cfde0
msfukui <msfukui@gmail.com>
2018-12-09 16:59:55 +0900

    Impl 'Privacy'

--- src/money.ts
+++ src/money.ts
@@ -1,16 +1,16 @@
 namespace Money {
   export class Dollar {
-    public amount: number = 0;
+    private amount: number = 0;
 
     constructor(amount: number) {
       this.amount = amount;
     }
 
-    times(multiplier: number): Dollar {
+    public times(multiplier: number): Dollar {
       return new Dollar(this.amount * multiplier);
     }
 
-    equals(object: any): boolean {
+    public equals(object: any): boolean {
       const dollar = object as Dollar;
       return this.amount === dollar.amount;
     }
--- test/money.test.ts
+++ test/money.test.ts
@@ -1,13 +1,11 @@
 import assert = require("assert");
 import Money from "../src/money";
 
-describe("Money", () => {
+describe("Money module", () => {
   it("Multiplication Test", () => {
     let five: Money.Dollar = new Money.Dollar(5);
-    let product: Money.Dollar = five.times(2);
-    assert(10 === product.amount);
-    product = five.times(3);
-    assert(15 === product.amount);
+    assert(five.times(2).equals(new Money.Dollar(10)));
+    assert(five.times(3).equals(new Money.Dollar(15)));
   });
 
   it("Equality Test", () => {
