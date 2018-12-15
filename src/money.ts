1fcad34d2afef0c679bc240a6d5e611620011fc6
msfukui <msfukui@gmail.com>
2018-12-15 13:05:09 +0900

    Impl 'The Root of All Evil'

--- src/money.ts
+++ src/money.ts
@@ -1,48 +1,38 @@
-namespace Money {
-  export class Money {
-    protected amount: number = 0;
-    protected cur: string = "";
+export class Money {
+  protected amount: number = 0;
+  protected cur: string = "";
 
-    constructor(amount: number, currency: string) {
-      this.amount = amount;
-      this.cur = currency;
-    }
-
-    public times(multiplier: number): Money {
-      return new Money(this.amount * multiplier, this.cur);
-    }
-
-    public currency(): string {
-      return this.cur;
-    }
-
-    public equals(object: any): boolean {
-      const money = object as Money;
-      return (
-        this.amount === money.amount &&
-        this.currency() === money.currency()
-      );
-    }
-
-    static dollar(amount: number): Money {
-      return new Dollar(amount, "USD");
-    }
-
-    static franc(amount: number): Money {
-      return new Franc(amount, "CHF");
-    }
+  constructor(amount: number, currency: string) {
+    this.amount = amount;
+    this.cur = currency;
   }
 
-  export class Dollar extends Money {
-    constructor(amount: number, currency: string) {
-      super(amount, currency);
-    }
+  public times(multiplier: number): Money {
+    return new Money(this.amount * multiplier, this.cur);
   }
 
-  export class Franc extends Money {
-    constructor(amount: number, currency: string) {
-      super(amount, currency);
-    }
+  public currency(): string {
+    return this.cur;
+  }
+
+  public equals(object: any): boolean {
+    const money = object as Money;
+    return (
+      this.amount === money.amount &&
+      this.currency() === money.currency()
+    );
+  }
+
+  public toString(): string {
+    return this.amount + " " + this.cur;
+  }
+
+  static dollar(amount: number): Money {
+    return new Money(amount, "USD");
+  }
+
+  static franc(amount: number): Money {
+    return new Money(amount, "CHF");
   }
 }
 
