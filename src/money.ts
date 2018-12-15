adcf876500cf3c3d0c156ccd79b5024b4f6e9268
msfukui <msfukui@gmail.com>
2018-12-15 22:34:07 +0900

    Impl 'Change'

--- src/money.ts
+++ src/money.ts
@@ -1,11 +1,29 @@
 namespace Money {
   interface Expression {
-    reduce(to: string): Money;
+    reduce(bank: Bank, to: string): Money;
   }
 
   export class Bank {
-    reduce(source: Expression, to: string): Money {
-      return source.reduce(to);
+    private rates = new Map<number, number>();
+
+    public reduce(source: Expression, to: string): Money {
+      return source.reduce(this, to);
+    }
+
+    public addRate(from: string, to: string, rate: number): void {
+      this.rates.set(new Pair(from, to).hashCode(), rate);
+      return;
+    }
+
+    public rate(from: string, to: string): number {
+      if (from === to) {
+        return 1;
+      }
+      const rate = this.rates.get(new Pair(from, to).hashCode());
+      if (!rate) {
+        return 0;
+      }
+      return rate;
     }
   }
 
@@ -18,8 +36,8 @@ namespace Money {
       this.addend = addend;
     }
 
-    public reduce(to: string) {
-      let amount = this.augend.getAmount() + this.addend.getAmount();
+    public reduce(bank: Bank, to: string) {
+      const amount = this.augend.getAmount() + this.addend.getAmount();
       return new Money(amount, to);
     }
   }
@@ -41,8 +59,9 @@ namespace Money {
       return new Sum(this, addend);
     }
 
-    public reduce(to: string) {
-      return this;
+    public reduce(bank: Bank, to: string) {
+      const rate = bank.rate(this.cur, to);
+      return new Money(this.amount / rate, to);
     }
 
     public currency(): string {
@@ -52,8 +71,7 @@ namespace Money {
     public equals(object: any): boolean {
       const money = object as Money;
       return (
-        this.amount === money.amount &&
-        this.currency() === money.currency()
+        this.amount === money.amount && this.currency() === money.currency()
       );
     }
 
@@ -71,6 +89,25 @@ namespace Money {
 
     static franc(amount: number): Money {
       return new Money(amount, "CHF");
+    }
+  }
+
+  class Pair {
+    private from: string;
+    private to: string;
+
+    constructor(from: string, to: string) {
+      this.from = from;
+      this.to = to;
+    }
+
+    public equals(object: any): boolean {
+      const pair = object as Pair;
+      return this.from === pair.from && this.to === pair.to;
+    }
+
+    public hashCode(): number {
+      return 0;
     }
   }
 }
--- test/money.test.ts
+++ test/money.test.ts
@@ -3,7 +3,7 @@ import Money from "../src/money";
 
 describe("Money module", () => {
   it("Multiplication Test", () => {
-    let five = Money.Money.dollar(5);
+    const five = Money.Money.dollar(5);
     assert(five.times(2).equals(Money.Money.dollar(10)));
     assert(five.times(3).equals(Money.Money.dollar(15)));
   });
@@ -20,31 +20,42 @@ describe("Money module", () => {
   });
 
   it("Simple Addition Test", () => {
-    let five = Money.Money.dollar(5);
-    let sum = five.plus(five);
-    let bank = new Money.Bank();
-    let reduced = bank.reduce(sum, "USD");
+    const five = Money.Money.dollar(5);
+    const sum = five.plus(five);
+    const bank = new Money.Bank();
+    const reduced = bank.reduce(sum, "USD");
     assert(Money.Money.dollar(10).equals(reduced));
   });
 
   it("Plus Returns Sum", () => {
-    let five = Money.Money.dollar(5);
-    let result = five.plus(five);
-    let sum = result as Money.Sum;
+    const five = Money.Money.dollar(5);
+    const result = five.plus(five);
+    const sum = result as Money.Sum;
     assert(five.equals(sum.augend));
     assert(five.equals(sum.addend));
   });
 
   it("Sum Reduce Test", () => {
-    let sum = new Money.Sum(Money.Money.dollar(3), Money.Money.dollar(4));
-    let bank = new Money.Bank();
-    let result = bank.reduce(sum, "USD");
+    const sum = new Money.Sum(Money.Money.dollar(3), Money.Money.dollar(4));
+    const bank = new Money.Bank();
+    const result = bank.reduce(sum, "USD");
     assert(Money.Money.dollar(7).equals(result));
   });
 
   it("Money Reduce Test", () => {
-    let bank = new Money.Bank(); 
-    let result = bank.reduce(Money.Money.dollar(1), "USD");
+    const bank = new Money.Bank(); 
+    const result = bank.reduce(Money.Money.dollar(1), "USD");
     assert(Money.Money.dollar(1).equals(result));
+  });
+
+  it("Reduce Money Different Currency Test", () => {
+    const bank = new Money.Bank();
+    bank.addRate("CHF", "USD", 2);
+    const result = bank.reduce(Money.Money.franc(2), "USD");
+    assert(Money.Money.dollar(1).equals(result));
+  });
+
+  it("identity Rate Test", () => {
+    assert(1 === new Money.Bank().rate("USD", "USD"));
   });
 });
