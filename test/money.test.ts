40b144b2492c92e57802d56b9c55b0f8aed89f4b
msfukui <msfukui@gmail.com>
2018-12-16 16:19:53 +0900

    Impl 'Abstraction, Finally'

--- test/money.test.ts
+++ test/money.test.ts
@@ -60,11 +60,31 @@ describe("Money module", () => {
   });
 
   it("Mixed Addition Test", () => {
-    const fiveBucks: Money.Money = Money.Money.dollar(5);
-    const tenFrancs: Money.Expression = Money.Money.franc(10);
+    const fiveBucks = Money.Money.dollar(5);
+    const tenFrancs = Money.Money.franc(10);
     const bank = new Money.Bank();
     bank.addRate("CHF", "USD", 2);
     const result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
     assert(Money.Money.dollar(10).equals(result));
+  });
+
+  it("Sum Plus Money Test", () => {
+    const fiveBucks = Money.Money.dollar(5);
+    const tenFrancs = Money.Money.franc(10);
+    const bank = new Money.Bank();
+    bank.addRate("CHF", "USD", 2);
+    const sum = new Money.Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    const result = bank.reduce(sum, "USD");
+    assert(Money.Money.dollar(15).equals(result));
+  });
+
+  it("Sum Times Test", () => {
+    const fiveBucks = Money.Money.dollar(5);
+    const tenFrancs = Money.Money.franc(10);
+    const bank = new Money.Bank();
+    bank.addRate("CHF", "USD", 2);
+    const sum = new Money.Sum(fiveBucks, tenFrancs).times(2);
+    const result = bank.reduce(sum, "USD");
+    assert(Money.Money.dollar(20).equals(result));
   });
 });
