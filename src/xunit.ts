28ced7a7b91683829e207ac80e8ca30128515754
msfukui <msfukui@gmail.com>
2018-12-24 17:28:47 +0900

    Impl 'Cleaning Up After'

--- src/xunit.ts
+++ src/xunit.ts
@@ -1,56 +1,50 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 class TestCase {
-  protected name: string = '';
+  protected name: string = "";
 
   constructor(name: string) {
     this.name = name;
   }
 
-  public setUp(): void {
-  }
+  public setUp(): void { }
+
+  public tearDown(): void { }
 
   public run(): void {
     this.setUp();
-    const method = 'this.' + this.name + '()';
+    const method = "this." + this.name + "()";
     eval(method);
+    this.tearDown();
   }
 }
 
 class WasRun extends TestCase {
-  public wasRun: boolean = false;
-  public wasSetUp: boolean = false;
+  public log: string = "";
 
   constructor(name: string) {
     super(name);
   }
 
   public testMethod(): void {
-    this.wasRun = true;
+    this.log = this.log + "testMethod ";
   }
 
   public setUp(): void {
-    this.wasSetUp = true;
+    this.log = "setUp ";
+  }
+
+  public tearDown(): void {
+    this.log = this.log + "tearDown";
   }
 }
 
 class TestCaseTest extends TestCase {
-   public test: any = null;
-
-  public setUp() {
-    this.test = new WasRun('testMethod');
-  }
-
-  public testRunning() {
-    this.test.run();
-    assert.ok(this.test.wasRun);
-  }
-
-  public testSetUp() {
-    this.test.run();
-    assert.ok(this.test.wasSetUp);
+  public testTemplateMethod() {
+    const test = new WasRun("testMethod");
+    test.run();
+    assert.ok("setUp testMethod tearDown" === test.log);
   }
 }
 
-new TestCaseTest('testRunning').run();
-new TestCaseTest('testSetUp').run();
+new TestCaseTest("testTemplateMethod").run();
